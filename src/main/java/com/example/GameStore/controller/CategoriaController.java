8118df53e23e5619203688bdeb2e6984f9894965
FRANK <diegoarellano680@gmail.com>
2024-11-19 00:12:54 -0500

    frank_intento_1

--- src/main/java/com/example/GameStore/controller/CategoriaController.java
+++ src/main/java/com/example/GameStore/controller/CategoriaController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CategoriaController {
-
+    /*-------*/
     @Autowired
     private CategoriaService service;
 
